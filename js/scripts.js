ebecf10857e80e5adb81a655c610f00721c475b2
mariozenmedina <mazen.mario@gmail.com>
2024-01-12 02:54:57 -0300

    backup

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const imgUrl = './img/exemplo2.jpg';
+const imgUrl = './img/exemplo.jpg';
 
 let img;
 
@@ -11,6 +11,29 @@ new zenImage(imgUrl).then( _ => {
     const ctx = canvas.getContext('2d');
     ctx.drawImage(_.img, 0, 0);
 
-    pixelArr = ctx.getImageData(0, 0, _.img.width, _.img.height);
-    img.histogram = pixelArr.data;
+    let pixelArr = ctx.getImageData(0, 0, _.img.width, _.img.height);
+    img.pixelArr = pixelArr.data;
+
+    img.renderHistogram();
+    img.showHistogram();
+
+    //Constrast Stretching
+    for(var i=0;i<img.pixelArr.length;i+=4){
+        let pOut = (img.oneChannel[i/4] - 40) * (255 / (150 - 40) ) + 0 || 0;
+        pOut = pOut < 0 ? 0 : Math.floor(pOut);
+        img.pixelArr[i] = 255 - pOut;
+        img.pixelArr[i+1] = 255 - pOut;
+        img.pixelArr[i+2] = 255 - pOut;
+        img.pixelArr[i+3] = 255;
+    }
+
+    ctx.clearRect(0,0,1000,1000);
+
+    //Generate image
+    let iData = new ImageData(img.pixelArr, 1000, 1000);
+    ctx.putImageData(iData, 0, 0);
+
+    img.renderHistogram();
+    img.showHistogram();
+    
 });
--- js/zenImage.class.js
+++ js/zenImage.class.js
@@ -1,6 +1,8 @@
 class zenImage {
 
     img;
+    pixelArr = [];
+    oneChannel = [];
     histogram = [];
 
     constructor(url) {
@@ -13,5 +15,61 @@ class zenImage {
             }
             img.src = url;
         });
+    }
+
+    renderHistogram() {
+        //OneChannel
+        this.oneChannel = [];
+        for(var i=0;i<this.pixelArr.length;i+=4){
+            this.oneChannel[i/4] = Math.round(this.pixelArr[i]*.299 + this.pixelArr[i+1]*.587 + this.pixelArr[i+2]*.114);
+        }
+
+        //Histogram
+        this.histogram = [];
+        for(var i=0;i<this.oneChannel.length;i++){
+            this.histogram[this.oneChannel[i]] = this.histogram[this.oneChannel[i]]+1 || 1;
+        }
+    }
+
+    showHistogram() {
+        const svg = d3.select('#histograma');
+        svg.attr('width', 256 * 3);
+        svg.attr('height', 600);
+        const rects = svg.selectAll('rect').data(this.histogram);
+        const maximo = d3.max(this.histogram);
+        rects.enter()
+            .append('rect')
+        .merge(rects)
+            .attr('width', 3)
+            .attr('height', (d, i) => d / maximo * 600 || 0)
+            .attr('x', (d, i) => i*3)
+            .attr('y', d => 600 - d / maximo * 600 || 0);
+        rects.exit().remove();
+    }
+
+    showCumulativeHistogram() {
+        let sum = 0;
+        const svg = d3.select('#histograma');
+        svg.attr('width', 256 * 3);
+        svg.attr('height', 600);
+        const rects = svg.selectAll('rect').data(this.histogram);
+        const maximo = this.histogram.reduce( (a, b) => a + b );
+        rects.enter()
+            .append('rect')
+        .merge(rects)
+            .attr('width', 3)
+            .attr('height', (d, i) => {
+                if(d){ sum += d };
+                const value = sum / maximo * 600 || 0;
+                return value;
+            })
+            .attr('x', (d, i) => i*3)
+            .attr('y', (d, i) => {
+                if(i==0){ sum = 0; }
+                if(d){ sum += d };
+                const value = 600 - sum / maximo * 600 || 0;
+                return value;
+            });
+        rects.exit().remove();
     }
 }
